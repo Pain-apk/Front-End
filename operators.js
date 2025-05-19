458b11c1191c40fc5dc4642439cfb9da864d6edc
Aditya <adi465996@gmail.com>
2025-05-19 08:49:41 +0530

    updated operators

--- operators.js
+++ operators.js
@@ -1,6 +1,6 @@
 //operators allows you to test qualities of varibles
 //there are 5 types of operators
-//1.Arithmetic operators(+,-,*,/,**,%,++)
+//1.Arithmetic operators(+,-,*,/,**,%,++,--)
 //2.Assignment Operators(=,+=,-=,*=,/=)
 //3.Comparison Operators(==,===,!=,!==,>,<,>=,<=)
 //4.Logical Operators(&&,||,!)
