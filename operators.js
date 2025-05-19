912653d0c86f5e427c8b65712c378dca55be8805
Aditya <adi465996@gmail.com>
2025-05-19 08:53:51 +0530

    changes in operators

--- operators.js
+++ operators.js
@@ -2,7 +2,7 @@
 //there are 5 types of operators
 //1.Arithmetic operators(+,-,*,/,**,%,++,--)
 //2.Assignment Operators(=,+=,-=,*=,/=)
-//3.Comparison Operators(==,===,!=,!==,>,<,>=,<=)
+//3.Comparison Operators(==,===,!=,!==,>,<,>=,<=) - used to compare two values and return a boolean result
 //4.Logical Operators(&&,||,!)
 //5.Bitwise Operators(&,|,^,~,<<,>>)
 //6.Ternary Operators( ? : )
