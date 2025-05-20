5c6795be57c7373a02ac917ad3896eedcf6588d0
Aditya <adi465996@gmail.com>
2025-05-20 12:18:47 +0530

    to prevent a confusing output we commented most of console.log statements

--- variable&object.js
+++ variable&object.js
@@ -8,25 +8,25 @@ var c = 40;
 String to number */
 const str = "123";
 const num = Number(str);
- console.log(typeof num);
+ //console.log(typeof num);
  //number to string
- const num2 = 456;
- const str2 = String(num2);
- console.log(typeof str2);
+const num2 = 456;
+const str2 = String(num2);
+ //console.log(typeof str2);
  //number to boolean
- const num3 = 0;
- const bool = Boolean(num3);
+const num3 = 0;
+const bool = Boolean(num3);
  /* "33" => 33
  "33abs" => NaN
  null => 0
  */
- console.log(typeof bool);
+ //console.log(typeof bool);
  //old way of declaring variables
 const myArray = [1,2,3,4,5];
  object1 = {
     name: "Aditya",
-    age: 20,
-    isStudent: true,
+   age: 20,
+  isStudent: true,
 
 }
 nestedObject = {
@@ -34,21 +34,25 @@ nestedObject = {
     age: 20,
     isStudent: true,
     address: {
-        city: "Nagpur",
-        state: "maharastra",
-        country: "India",
-    }
+  city: "Nagpur",
+    state: "maharastra",
+      country: "India",
+  }
 }
-console.log(myArray.slice(0,4));
-console.log(a);
-console.log(b);
-console.log(c);
+//console.log(myArray.slice(0,4));
+//console.log(a);
+//console.log(b);
+//console.log(c);
 for(i=0;i<myArray.length;i++){
-    console.log(myArray[i]);
+  //  console.log(myArray[i]);
 
 }
 /*ways to print arrays, objects, nested objects
   and variables we can also pass their values to varuables*/
-console.log(object1.name);
-console.log(nestedObject.address.city);
-console.log(nestedObject['address']['state']);
+//console.log(object1.name);
+//console.log(nestedObject.address.city);
+//console.log(nestedObject['address']['state']);
+//console.log(1+2+"2");
+//console.log("1"+2+2);
+//both will print different output and to prevent these
+// we use () parenthesis inorder to seperate the values
